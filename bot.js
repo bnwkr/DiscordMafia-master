17b0278596a6daf03c6798e4af50799257f139f8
Ben Walker <thehivix@gmail.com>
2017-01-28 23:38:33 +0000

    added start_bot.bat and bot.js - v1

---
+++ bot.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('Successfully started!');
+});
+
+client.login('Mjc1MDM2OTM1NDM1NzE0NTYw.C260Tg.sf8HByrnOnM2uk6VkRQKTK6YJug');

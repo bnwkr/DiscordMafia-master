43f0eaebf8097479ec2300e802f451b1249a0ccb
Tiemen <ThaTiemsz@users.noreply.github.com>
2017-01-29 02:22:37 +0100

    Add check if game is full

--- bot.js
+++ bot.js
@@ -13,12 +13,16 @@ client.on('message', message => {
 	}
 
 	if(message.content === '!join') {
-		if(message.member.roles.has('275059795189563393')){
-      message.delete();
-      message.reply('you are already in the game!');
-    } else {
-		message.member.addRole('275059795189563393');
-		message.channel.sendMessage(message.author.username + " has joined `Game1!`");
-  	}
-   }
+		if(message.member.roles.has('275059795189563393')) {
+			message.delete();
+			message.reply('you are already in the game!');
+		} else {
+			if(message.guild.roles.get('275059795189563393').members.size === 12) {
+				message.reply('this game is full!');
+			} else {
+				message.member.addRole('275059795189563393');
+				message.channel.sendMessage(message.author.username + " has joined `Game1!`");
+			}
+		}
+	}
 });

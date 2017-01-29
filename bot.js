d3c31407486cf4c775bc149e4ea0bd35379c078e
Ben Walker <BlueDeadpool@users.noreply.github.com>
2017-01-29 00:17:56 +0000

    Add files via upload

--- bot.js
+++ bot.js
@@ -5,4 +5,10 @@ client.on('ready', () => {
     console.log('Successfully started!');
 });
 
-client.login('token');
+client.on('message', message => {
+  if(message.content === '!create'){
+      message.channel.sendMessage('**Game 1 is starting in** `5` **minutes! Type** `!join` **to enter!**');
+  }
+});
+
+client.login('Mjc1MDM2OTM1NDM1NzE0NTYw.C26-aA.JLuZlzc8k6tKHA64wiS0thW5pNU');

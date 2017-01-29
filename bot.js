b9d1ddabb361851959e0d33793c2cbd31012a550
Tiemen <Tiemen2@live.nl>
2017-01-29 06:13:34 +0100

    Another test commit

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ client.login('token');
 client.on('ready', () => {
 	console.log('Successfully started!');
 });
-
+// another test
 const game1Lobby = '275059795189563393';
 const game1 = '274976378867154965';
 const staff = '274986888832614401';

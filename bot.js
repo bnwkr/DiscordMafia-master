a07444d66e165dcc9ced37fa15fcb8af4b0b1a07
Ben Walker <BlueDeadpool@users.noreply.github.com>
2017-01-29 00:46:18 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,13 @@ client.on('ready', () => {
 
 client.on('message', message => {
   if(message.content === '!create'){
-      message.channel.sendMessage('**Game 1 is starting in** `5` **minutes! Type** `!join` **to enter!**');
+    message.channel.sendMessage('**Game 1 is starting in** `5` **minutes! Type** `!join` **to enter!**');
   }
 });
 
+client.on('message', message => {
+  if(message.content === '!join'){
+    #WE CAN'T GET THIS RIGHT ARGHHHH
+    message.channel.sendMessage(message.author.username + 'has joined Game1!');
+  }
+});

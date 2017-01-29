0d199a4b1db12530f4d8c332d24ee4a8955889c5
Ben Walker <BlueDeadpool@users.noreply.github.com>
2017-01-29 01:43:09 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,22 +7,31 @@ client.on('ready', () => {
 	console.log('Successfully started!');
 });
 
+var game1Lobby = '275059795189563393';
+var game1 = '275078289108762624';
+
 client.on('message', message => {
 	if(message.content === '!create') {
 		message.channel.sendMessage('**Game 1 is starting in** `5` **minutes! Type** `!join` **to enter!**');
 	}
 
 	if(message.content === '!join') {
-		if(message.member.roles.has('275059795189563393')) {
+		if(message.member.roles.has(game1Lobby)) {
 			message.delete();
 			message.reply('you are already in the game!');
 		} else {
-			if(message.guild.roles.get('275059795189563393').members.size === 12) {
+			if(message.guild.roles.get(game1Lobby).members.size === 12) {
 				message.channel.sendMessage("`Game 1` is now full!");
 			} else {
-				message.member.addRole('275059795189563393');
+				message.member.addRole(game1Lobby);
 				message.channel.sendMessage(message.author.username + " has joined `Game1!`");
 			}
 		}
 	}
+
+  if(message.content === '!start') {
+    if(message.member.roles.has(game1Lobby)) {
+      message.member.addRole(game1);
+    }
+  }
 });

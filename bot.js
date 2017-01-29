e05d954927b8b315faa14ee53469f87e71d30a55
Tiemen <ThaTiemsz@users.noreply.github.com>
2017-01-29 02:01:05 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,18 +4,16 @@ const client = new Discord.Client();
 client.login('token');
 
 client.on('ready', () => {
-    console.log('Successfully started!');
+	console.log('Successfully started!');
 });
 
 client.on('message', message => {
-  if(message.content === '!create'){
-    message.channel.sendMessage('**Game 1 is starting in** `5` **minutes! Type** `!join` **to enter!**');
-  }
-});
-
-client.on('message', message => {
-  if(message.content === '!join'){
-    //WE CAN'T GET THIS RIGHT ARGHHHH//
-    message.channel.sendMessage(message.author.username + 'has joined Game1!');
-  }
+	if(message.content === '!create') {
+		message.channel.sendMessage('**Game 1 is starting in** `5` **minutes! Type** `!join` **to enter!**');
+	}
+	
+	if(message.content === '!join') {
+		message.member.addRole('275059795189563393');
+		message.channel.sendMessage(message.author.username + 'has joined Game1!');
+  	}
 });
